24000d4954fdf2184649fee51d6d10bd2c5f6b55
Vincent GIBAULT <vincent.gibault@softeam.fr>
2020-09-04 00:15:42 +0200

    adding search controller

---
+++ Blazor.Startechmanager.Server.UnitTests/SearchUserControllerTests.cs
@@ -0,0 +1,71 @@
+﻿using Blazor.Startechmanager.Server.Controllers;
+using Blazor.Startechmanager.Server.Models;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Startechmanager.Server.UnitTests
+{
+    public class SearchUserControllerTests : BaseTests<SearchUserController>
+    {
+        public void PopulateDatas()
+        {
+            for(int n = 0; n < 15; ++n)
+            {
+                DbContext.Users.Add(new ApplicationUser
+                {
+                    Id = 100 + n,
+                    UserName = $"test_abc_{n}"
+                });
+            }
+
+            DbContext.Users.Add(new ApplicationUser
+            {
+                Id = 1,
+                UserName = "totoabctiti"
+            });
+
+            DbContext.Users.Add(new ApplicationUser
+            {
+                Id = 2,
+                UserName = "abctutu"
+            });
+            DbContext.Users.Add(new ApplicationUser
+            {
+                Id = 3,
+                UserName = "zozototoabc"
+            });
+            DbContext.Users.Add(new ApplicationUser
+            {
+                Id = 3,
+                UserName = "youpladefghil"
+            });
+
+            DbContext.SaveChanges();
+        }
+
+        public async Task should_not_return_a_user_if_their_is_less_than_3_characters()
+        {
+            var target = Create();
+            var result = await target.Search("ab");
+            result.Should().BeEmpty();
+        }
+
+        public async Task should_return_the_user_who_match_the_like_pattern()
+        {
+            var target = Create();
+            var result = await target.Search("abc");
+            result.Select(x => x.UserName).Should().BeEquivalentTo("totoabctiti", "abctutu", "zozototoabc");
+        }
+
+        public async Task should_return_a_least_10_values()
+        {
+            var target = Create();
+            var result = await target.Search("test");
+            result.Should().HaveCount(10);
+        }
+    }
+}
---
+++ Server/Controllers/SearchUserController.cs
@@ -0,0 +1,37 @@
+﻿using Blazor.Startechmanager.Server.Data;
+using Blazor.Startechmanager.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Startechmanager.Server.Controllers
+{
+    public class SearchUserController
+    {
+        public SearchUserController(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public ApplicationDbContext DbContext { get; set; }
+
+        [HttpPost("SearchUser")]
+        public async Task<IList<UserObject>> Search(string userToSearch)
+        {
+            if(userToSearch?.Length < 3)
+            {
+                return Array.Empty<UserObject>();
+            }
+
+#pragma warning disable CS8604 // Possible null reference argument.
+            return await DbContext.Users.Where(x => x.UserName.Contains(userToSearch)).Take(10)
+#pragma warning restore CS8604 // Possible null reference argument.
+                .Select(x => new UserObject { Id = x.Id, UserName = x.UserName }).ToListAsync();
+        }
+    }
+}
