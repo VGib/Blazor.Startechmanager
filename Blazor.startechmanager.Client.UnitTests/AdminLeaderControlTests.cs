37b09b17f607ddea650abe4119e8670f3fa9020c
Vincent GIBAULT <vincent.gibault@softeam.fr>
2020-09-10 12:02:40 +0200

    add unit test for AmdinLeaderControl

--- Blazor.startechmanager.Client.UnitTests/AdminLeaderControlTests.cs
+++ Blazor.startechmanager.Client.UnitTests/AdminLeaderControlTests.cs
@@ -1,23 +1,21 @@
-﻿using Blazor.Startechmanager.Client.Component;
+﻿using AngleSharp.Dom;
+using Blazor.Startechmanager.Client.Component;
 using Blazor.Startechmanager.Shared.Models;
 using Bunit;
 using Bunit.Rendering;
-using Castle.DynamicProxy.Contributors;
-using Common.UnitTests;
-using Moq;
 using NUnit.Framework;
 using RichardSzalay.MockHttp;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
 using System.Text.Json;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Blazor.startechmanager.Client.UnitTests
 {
+
     public class AdminLeaderControlTests : BaseTestsForComponent<AdminLeaderControl>
     { 
 
@@ -25,11 +23,16 @@ namespace Blazor.startechmanager.Client.UnitTests
 
         public ComponentParameter DisplayName { get; } = ComponentParameterFactory.Parameter(nameof(AdminLeaderControl.DisplayName), "Administrator");
 
+        public IEnumerable<UserObject> Users { get; } = new[] { new UserObject { UserName = "Admin", Id =  1 } };
+
 
         [Test]
-        public void when_loading_leaders_the_razor_component_should_be_refreshed_with_leader()
+        public async Task when_loading_leaders_the_razor_component_should_be_refreshed_with_leader()
         {
-            throw new NotImplementedException("to do");
+            MockHttp.Expect(HttpMethod.Get, "http://localhost/StartechLeader/Admin/GetLeaders")
+                .Respond("application/json", JsonSerializer.Serialize(new [] { new UserObject { UserName = "jsqdklqdqs" } }));
+            var target = CreateComponent(StartechType, DisplayName);
+            target.WaitForAssertion( () => target.Nodes.QuerySelectorAll("li:contains(\"jsqdklqdqs\")").Any(), TimeSpan.FromMilliseconds(30));
         }
 
         [Test]
@@ -43,27 +46,53 @@ namespace Blazor.startechmanager.Client.UnitTests
         }
 
         [Test]
-        public void when_adding_a_leader_the_right_startechtype_api_should_be_called()
+        public async Task when_adding_a_leader_the_right_startechtype_api_should_be_called()
         {
-            throw new NotImplementedException("to do");
+            MockHttp.Expect(HttpMethod.Get, "http://localhost/StartechLeader/Admin/GetLeaders").Respond("application/json", JsonSerializer.Serialize(Users));
+            var target = CreateComponent(StartechType, DisplayName);
+            await Task.Delay(30);
+            var userObjectToAdd = new UserObject { Id = 5,  UserName =  "toAdd13215" };
+            target.Instance.UserObjectToAdd = userObjectToAdd;
+            MockHttp.Expect(HttpMethod.Get, "http://localhost/StartechLeader/Admin/AddLeader/5").Respond(HttpStatusCode.OK);
+            target.Instance.AddUser();
+            MockHttp.VerifyNoOutstandingExpectation();
         }
 
         [Test]
-        public void when_removing_a_leader_the_right_startechtype_api_should_be_called()
+        public async Task when_removing_a_leader_the_right_startechtype_api_should_be_called()
         {
-            throw new NotImplementedException("to do");
+            MockHttp.Expect(HttpMethod.Get, "http://localhost/StartechLeader/Admin/GetLeaders").Respond("application/json", JsonSerializer.Serialize(Users));
+            var target = CreateComponent(StartechType, DisplayName);
+            await Task.Delay(30);
+            MockHttp.Expect(HttpMethod.Get, "http://localhost/StartechLeader/Admin/RemoveLeader/1").Respond(HttpStatusCode.OK);
+            target.Instance.OnRemove(Users.First());
+            MockHttp.VerifyNoOutstandingExpectation();
         }
 
         [Test]
-        public void when_adding_a_leader_the_leader_list_should_be_reload()
+        public async Task when_adding_a_leader_the_leader_list_should_be_reload()
         {
-            throw new NotImplementedException("to do");
+            MockHttp.Expect(HttpMethod.Get, "http://localhost/StartechLeader/Admin/GetLeaders").Respond("application/json", JsonSerializer.Serialize(Users));
+            var target = CreateComponent(StartechType, DisplayName);
+            await Task.Delay(30);
+            var userObjectToAdd = new UserObject { Id = 5, UserName = "toAdd13215" };
+            target.Instance.UserObjectToAdd = userObjectToAdd;
+            MockHttp.Expect(HttpMethod.Get, "http://localhost/StartechLeader/Admin/AddLeader/5").Respond(HttpStatusCode.OK);
+            MockHttp.Expect(HttpMethod.Get, "http://localhost/StartechLeader/Admin/GetLeaders").Respond("application/json", JsonSerializer.Serialize(Users.Union(new[] { userObjectToAdd})));
+            target.Instance.AddUser();
+            target.WaitForAssertion(() => target.Nodes.QuerySelectorAll("li:contains(\"toAdd13215\")").Empty(), TimeSpan.FromMilliseconds(30));
         }
 
         [Test]
-        public void when_removing_a_leader_the_leader_list_should_be_called()
+        public async Task when_removing_a_leader_the_leader_list_should_be_called()
         {
-            throw new NotImplementedException("to do");
+            MockHttp.Expect(HttpMethod.Get, "http://localhost/StartechLeader/Admin/GetLeaders").Respond("application/json", JsonSerializer.Serialize(Users));
+            var target = CreateComponent(StartechType, DisplayName);
+            await Task.Delay(30);
+            MockHttp.Expect(HttpMethod.Get, "http://localhost/StartechLeader/Admin/RemoveLeader/1").Respond(HttpStatusCode.OK);
+            MockHttp.Expect(HttpMethod.Get, "http://localhost/StartechLeader/Admin/GetLeaders").Respond("application/json", JsonSerializer.Serialize(new UserObject[0]));
+            target.Instance.OnRemove(Users.First());
+            target.WaitForAssertion(() => target.Nodes.QuerySelectorAll("li:contains(\"Admin\")").Empty(), TimeSpan.FromMilliseconds(30));
         }
     }
 }
