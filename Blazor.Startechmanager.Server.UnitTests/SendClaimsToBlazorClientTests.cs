fe814e3c7e7fdc65fdb0206356c2fba805456dae
Vincent GIBAULT <vincent.gibault@softeam.fr>
2020-09-14 11:00:09 +0200

    adding unit tests for sending claims to blazor client

---
+++ Blazor.Startechmanager.Server.UnitTests/SendClaimsToBlazorClientTests.cs
@@ -0,0 +1,33 @@
+﻿using Blazor.Startechmanager.Server.Services;
+using Blazor.Startechmanager.Shared.Models;
+using Blazor.Startechmanager.Shared.Policies;
+using FluentAssertions;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Blazor.Startechmanager.Server.UnitTests
+{
+    public class SendClaimsToBlazorClientTests
+    {
+        public IProfileService Target { get; set; } = new SendClaimsToBlazorClientProfileService();
+
+        [Test]
+        public async Task startech_claims_should_be_send_to_blazor_clients()
+        {
+            var claimToTransfert = new Claim(StartechClaimHelper.GetStartechClaimType(Startechs.Dotnet), StartechClaimHelper.Member);
+            var profileRequest = new ProfileDataRequestContext
+            {
+                Subject = new ClaimsPrincipal(new ClaimsIdentity(new[] { claimToTransfert }))
+            };
+            await Target.GetProfileDataAsync(profileRequest);
+            profileRequest.IssuedClaims.Select(x => x.Type).Should().BeEquivalentTo(claimToTransfert.Type);
+            profileRequest.IssuedClaims.Select(x => x.Value).Should().BeEquivalentTo(claimToTransfert.Value);
+        }
+    }
+}
