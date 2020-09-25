bebf24472cdd722bfab1fb12bb595422fe4c649b
Vincent GIBAULT <vincent.gibault@softeam.fr>
2020-09-25 10:48:11 +0200

    modifying starpointsItem

---
+++ Shared/Models/StarpointsItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Blazor.Startechmanager.Shared.Models
+{
+    public enum ValidationState
+    {
+        Validated = 1,
+        Refused = 2,
+        InStudy = 3
+    }
+
+    public class StarpointsItem
+    {
+        public int Id { get; set; }
+        public int ApplicationUserId { get; set; }
+        public DateTime Date { get; set; }
+        public int NumberOfPoints { get; set; }
+
+        public Startechs Startech { get; set; }
+
+        public ValidationState ValidationState { get; set; }
+
+        public string? UrlJustification { get; set; }
+
+        public string? TextJustification { get; set; }
+    }
+}
