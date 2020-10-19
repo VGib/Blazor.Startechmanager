41e7edd89e27e22ba3075dfff084edd90b124431
Vincent GIBAULT <vincent.gibault@softeam.fr>
2020-10-19 12:49:13 +0200

    working on fixing unit tests

--- Blazor.Startechmanager.Server.UnitTests/BaseTestsWithDbContext.cs
+++ Blazor.Startechmanager.Server.UnitTests/BaseTestsWithDbContext.cs
@@ -3,6 +3,7 @@ using Common.UnitTests;
 using EntityFrameworkCore.Testing.Moq.Helpers;
 using IdentityServer4.EntityFramework.Options;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -17,9 +18,13 @@ namespace Blazor.Startechmanager.Server.UnitTests
 
        [SetUp]
         public void SetUpDbContext()
-        { 
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddEntityFrameworkInMemoryDatabase();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
             DbContext = new MockedDbContextBuilder<ApplicationDbContext>().UseConstructorWithParameters(
-                new DbContextOptionsBuilder().UseInMemoryDatabase(Guid.NewGuid().ToString(), new InMemoryDatabaseRoot()).Options,
+                new DbContextOptionsBuilder().UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .UseInternalServiceProvider(serviceProvider).EnableServiceProviderCaching(false).Options,
                 new OptionsWrapper<OperationalStoreOptions>(new OperationalStoreOptions()))
                 .MockedDbContext;
             ServiceCollection.AddSingleton(_ => DbContext);
